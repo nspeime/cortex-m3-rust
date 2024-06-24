e527d3a3bc6fb6cc30082a9f665b89e87eee516f
nspeime <snpeime@gmail.com>
2024-06-24 13:54:14 +0800

    Demonstrate context switch using activate function
    Implement a context switch example using the activate function to
    switch to a user task. The user task prints "USER TASK 1" and is
    switched by the activate function.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 #![allow(unused)]
 #![no_main]
 #![no_std]
+use core::ptr;
 use core::panic::PanicInfo;
 mod uart;
+mod context_switch;
 use uart::{uart_init, uart_print};
+
+extern "C" {
+    fn activate(task_stack: *const u32);
+}
+
+fn usertask1() {
+    uart_print("USER TASK 1\n");
+    loop {}
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn Reset() -> ! {
     uart_init();
-    uart_print("Hello World\n");
+    let mut usertask_stack: [u32; 256] = [0; 256];
+    let usertask_stack_start = &mut usertask_stack[256 - 16] as *mut u32;
+    unsafe {
+        ptr::write_volatile(usertask_stack_start.offset(8), usertask1 as u32);
+        activate(usertask_stack_start);
+    }
     loop {}
 }
 
